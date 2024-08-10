d0ba8a9fdecbc292305dbc44bebc2f053422f911
iridescent <2389504513@qq.com>
2024-08-10 17:40:29 +0800

    ✨ feat(i18n): 测试LanguageDetector配置

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,12 +1,20 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18next
+  .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: 'zh',// 默认语言
+    // lng: 'zh',// 默认语言
     fallbackLng: 'zh', // 匹配不上，提供兜底默认语言
-    fallbackNS: 'common', // 默认命名空间，不提供则需要手动指定，否则无法翻译
+    fallbackNS: 'common', // 默认命名空间，不提供则需要手动指定，否则无法翻译,
+    /** 配置 LanguageDetector */
+    detection: {
+      order: ['htmlTag']
+    },
     resources: {
       'zh': {
         common: {
