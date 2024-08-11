a89f99a4e307f3157e678268e45a7b9a3c28245c
iridescent <2389504513@qq.com>
2024-08-11 20:25:24 +0800

    ✨ feat(locales): 新增Backend支持

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -9,34 +9,39 @@ i18next
   .use(initReactI18next)
   .init({
     // lng: 'zh',// 默认语言
+    ns: ['common'], // 使用backend插件，需要显示配置命名空间，对应加载{{ns}}.json
     fallbackLng: 'zh', // 匹配不上，提供兜底默认语言
     fallbackNS: 'common', // 默认命名空间，不提供则需要手动指定，否则无法翻译,
     /** 配置 LanguageDetector */
     detection: {
       order: ['htmlTag']
     },
-    resources: {
-      'zh': {
-        common: {
-          title: '当前标题为 {{ title }} {{count}}',
-          'hello': '你好',
-          'world': '世界',
-          userInfo: {
-            name: '昵称',
-            age: '年龄'
-          }
-        }
-      },
-      'en': {
-        common: {
-          title: 'current title is {{title}} {{ count }}',
-          'hello': 'hello',
-          'world': 'world',
-          userInfo: {
-            name: 'name',
-            age: 'age'
-          }
-        }
-      }
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json', // 加载翻译文件的路径，以/开头代表访问的服务器资源路径，如 http://localhost:5173/locales/zh/common.json
+      addPath: '/locales/add/{{lng}}/{{ns}}', // 添加翻译文件的路径
     },
+    // resources: {
+    //   'zh': {
+    //     common: {
+    //       title: '当前标题为 {{ title }} {{count}}',
+    //       hello: '你好',
+    //       world: '世界',
+    //       userInfo: {
+    //         name: '昵称',
+    //         age: '年龄'
+    //       }
+    //     }
+    //   },
+    //   'en': {
+    //     common: {
+    //       title: 'current title is {{title}} {{ count }}',
+    //       hello: 'hello',
+    //       world: 'world',
+    //       userInfo: {
+    //         name: 'name',
+    //         age: 'age'
+    //       }
+    //     }
+    //   }
+    // },
   })
--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n/index'
+import { Suspense } from 'react'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Suspense fallback={<div>Loading...</div>}>
+    <App />
+  </Suspense>
+)
